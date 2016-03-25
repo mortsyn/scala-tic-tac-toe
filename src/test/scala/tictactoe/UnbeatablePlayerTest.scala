f3b3cbae14df592784b31ce37c1ac91e61a786b5
mortsyn <mortsynb@gmail.com>
2016-03-25 15:57:20 -0500

    Created UnbeatablePlayer

---
+++ src/test/scala/tictactoe/UnbeatablePlayerTest.scala
@@ -0,0 +1,28 @@
+package tictactoe
+
+import org.scalatest.{Matchers, FunSpec}
+
+class UnbeatablePlayerTest extends FunSpec with Matchers with BoardSpecHelper {
+  val computer = UnbeatableComputer
+
+  describe("unbeatable move generator") {
+
+    it("should block the opponents move") {
+      val board = createBoardStateFromMoves(Vector(0, 2, 6))
+
+      computer.getMove(board) should equal(3)
+    }
+
+    it("should return the middle board spot if corner is taken") {
+      val board = createBoardStateFromMoves(Vector(0))
+
+      computer.getMove(board) should equal(4)
+    }
+
+    it("should make the smart move in tough situations") {
+      val board = createBoardStateFromMoves(Vector(1, 6, 5, 7, 8))
+
+      computer.getMove(board) should equal(2)
+    }
+  }
+}
