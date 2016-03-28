b7520438e412b443e52af69986e045688018571f
mortsyn <mortsynb@gmail.com>
2016-03-28 12:11:41 -0500

    small changes

--- src/test/scala/tictactoe/IOTest.scala
+++ src/test/scala/tictactoe/IOTest.scala
@@ -10,6 +10,7 @@ import tictactoe.strategies.TicTacToe
 class IOTest extends FunSpec with Matchers {
 
   describe("input") {
+
     it("should ignore strings and other cruft") {
       val board = Board(3, TicTacToe).play(0)
       val human = Human(X, IO(new Scanner("0 sadfj adsf asdfjasdf 8")))
