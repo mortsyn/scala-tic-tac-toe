76b9fbb3287d7e8eb89b4c30f05896f4da90194e
mortsyn <mortsynb@gmail.com>
2016-03-30 19:59:48 -0500

    Human takes input

--- src/main/scala/tictactoe/players/Human.scala
+++ src/main/scala/tictactoe/players/Human.scala
@@ -1,12 +1,21 @@
 package tictactoe.players
 
-import tictactoe.players.tokens.{EMPTY, Token}
-import tictactoe.{Board, IO}
+import tictactoe.players.tokens.{EMPTY}
+import tictactoe.{Board}
+import java.util.Scanner
 
-case class Human() extends Player with IO {
+case class Human(input: Scanner = new Scanner(System.in)) extends Player {
+
+  def nextInt: Int = {
+    while (!input.hasNextInt()) {
+      input.next()
+    }
+
+    return input.nextInt()
+  }
 
   override def getMove(board: Board): Int = {
-    val move = this.nextInt
+    val move = nextInt
     if((move < board.state.size && move >= 0) && (board.state(move) == EMPTY)) move else getMove(board)
   }
 }
