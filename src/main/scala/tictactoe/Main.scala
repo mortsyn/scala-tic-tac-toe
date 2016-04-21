e96541f391ca40ab5729b4943079d12997a73bde
mortsyn <mortsynb@gmail.com>
2016-04-21 15:09:09 -0500

    Added main game loop tests

--- src/main/scala/tictactoe/Main.scala
+++ src/main/scala/tictactoe/Main.scala
@@ -1,5 +1,14 @@
 package tictactoe
 
 object Main {
-  def main(args: Array[String]): Unit = { }
+
+  def main(args: Array[String]): Unit = {
+    run(new UI(), Game())
+  }
+
+  def run(ui: UI, game: Game): Unit = {
+    ui.update(game)
+    if(!game.isOver) run(ui, game.makeMove(game.activePlayer.getMove(game)))
+  }
+
 }
